369809ea133386e35c76bdc897c1f8cc30400f1c
Muhammad Ausaf Jamal <ausaffarooqui17@gmail.com>
2024-07-23 13:53:56 +0500

    refactored Cube class to use colored cubelets

--- src/rubiksCube/classes/cube.js
+++ src/rubiksCube/classes/cube.js
@@ -2,59 +2,85 @@ import * as THREE from 'three';
 
 export class Cube {
     constructor() {
-        this.cubeGroup = new THREE.Group();
+        this.cube = new THREE.Group();
     }
 
     start(scene) {
-        const size = 5;
-        const cubeGeometry = new THREE.BoxGeometry(size, size, size)
 
-        const color = new THREE.Color();
-        const colors = []
-        for (let i = 0; i < cubeGeometry.attributes.position.count; i++) {
-            const r = Math.random()*5;
-            const g = Math.random()*5;
-            const b = Math.random()*5;
-            color.setRGB(r, g, b, THREE.SRGBColorSpace)
-            colors.push(r, g, b)
+        let cubelet;
+        const cubeSize = 5;
+
+        const rubiksCubeColors = [
+            "#0000FF", // Blue
+            "#FFA500",  // Orange
+            "#FFFFFF", // White
+            "#FF0000", // Red
+            "#FFFF00", // Yellow
+            "#00FF00", // Green
+        ];
+        let cubeGeometry;
+        function createColoredCube() {
+            cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize).toNonIndexed();
+            // const positionAttribute = cubeGeometry.getAttribute('position');
+            const colors = [];
+            const color = new THREE.Color();
+
+            // Loop through each face (6 faces, 6 vertices per face)
+            for (let face = 0; face < 6; face++) {
+                color.set(rubiksCubeColors[face]);
+
+                for (let vertex = 0; vertex < 6; vertex++) {
+                    colors.push(color.r, color.g, color.b);
+                }
+            }
+            cubeGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
         }
-
-        cubeGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-
+        createColoredCube()
 
         const positions = [
             { x: 0, y: 0, z: 0, color: 0xff0000 },
-            { x: 5.1, y: 0, z: 0, color: 0xffffff },
-            { x: 0, y: 5.1, z: 0, color: 0xffffff },
-            { x: 5.1, y: 5.1, z: 0, color: 0xff0000 },
-            { x: 0, y: 0, z: 5.1, color: 0xFFA500 },
-            { x: 5.1, y: 0, z: 5.1, color: 0xffff00 },
-            { x: 0, y: 5.1, z: 5.1, color: 0xffff00 },
-            { x: 5.1, y: 5.1, z: 5.1, color: 0xFFA500 },
+            { x: 5.03, y: 0, z: 0, color: 0xffffff },
+            { x: 0, y: 5.03, z: 0, color: 0xffffff },
+            { x: 5.03, y: 5.03, z: 0, color: 0xff0000 },
+            { x: 0, y: 0, z: 5.03, color: 0xFFA500 },
+            { x: 5.03, y: 0, z: 5.03, color: 0xffff00 },
+            { x: 0, y: 5.03, z: 5.03, color: 0xffff00 },
+            { x: 5.03, y: 5.03, z: 5.03, color: 0xFFA500 },
         ];
 
-        let cube;
-        positions.forEach(pos => {
-            cube = new THREE.Mesh(
+        const colorOrders = [
+            ["#FFFFFF", "#FF0000", "#FFFF00", "#0000FF", "#00FF00", "#FFA500"], // Cube 1
+            ["#FF0000", "#FFFF00", "#0000FF", "#00FF00", "#FFA500", "#FFFFFF"], // Cube 2
+            ["#FFFF00", "#0000FF", "#00FF00", "#FFA500", "#FFFFFF", "#FF0000"], // Cube 3
+            ["#0000FF", "#00FF00", "#FFA500", "#FFFFFF", "#FF0000", "#FFFF00"], // Cube 4
+            ["#00FF00", "#FFA500", "#FFFFFF", "#FF0000", "#FFFF00", "#0000FF"], // Cube 5
+            ["#FFA500", "#FFFFFF", "#FF0000", "#FFFF00", "#0000FF", "#00FF00"], // Cube 6
+            ["#FFFFFF", "#FFA500", "#00FF00", "#0000FF", "#FFFF00", "#FF0000"], // Cube 7
+            ["#FF0000", "#FFFFFF", "#FFA500", "#00FF00", "#0000FF", "#FFFF00"]  // Cube 8
+        ];
+
+        positions.forEach((pos, index) => {
+            cubelet = new THREE.Mesh(
                 cubeGeometry,
-                new THREE.MeshBasicMaterial({ color: pos.color, })
+                new THREE.MeshBasicMaterial({ color: colorOrders[index], vertexColors: true })
+
             );
-            cube.position.set(pos.x, pos.y, pos.z);
-            this.cubeGroup.add(cube);
-            scene.add(this.cubeGroup)
+            cubelet.position.set(pos.x, pos.y, pos.z);
+            this.cube.add(cubelet);
+            scene.add(this.cube)
         })
-
-        const edges = new THREE.EdgesGeometry(cubeGeometry);
-        // console.log(edges)
-        const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff, vertexColors: true }));
-        scene.add(line);
+        // const edges = new THREE.EdgesGeometry(cubeGeometry);
+        // const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+        // scene.add(line);
         // console.log(cubeGeometry)
+        // console.log(line)
 
-        const posArray = cube.geometry.attributes.position.array;
-        const indArray = cube.geometry.index.array;
+        // const posArray = cubeGeometry.attributes.position.array;
+        // const indArray = cubeGeometry.index.array;
 
-        console.log(posArray)
-        console.log(indArray)
+        // console.log(posArray)
+        // console.log(indArray)
 
 
         const helper = new THREE.AxesHelper(10)
--- src/rubiksCube/classes/world.js
+++ src/rubiksCube/classes/world.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import {
     INNER_HEIGHT, INNER_WIDTH, CAMERA_NEAR, CAMERA_FAR, ASPECT_RATIO, CAMERA_FOV,
-    ENABLE_DAMPING, DAMPING_FACTOR, MAX_POLAR_ANGLE, MIN_POLAR_ANGLE, MIN_DISTANCE, MAX_DISTANCE, ENABLE_PAN, CAMERA_INITIAL_POSITION, CAMERA_INITIAL_LOOK_AT
+    ENABLE_DAMPING, DAMPING_FACTOR, MIN_DISTANCE, MAX_DISTANCE, ENABLE_PAN, CAMERA_INITIAL_POSITION, CAMERA_INITIAL_LOOK_AT
 } from '../constants/common-constants.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Cube } from "./cube.js";
@@ -16,11 +16,12 @@ export default class World {
         this.renderer = new THREE.WebGLRenderer({ canvas: this.ref.current, antialias: true });
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-        // Renderer
+        // Renderer and Controls
         this.handleRendererConfigurations();
         this.handleControlsConfigurations();
         this.handleResponsiveness();
         this.animate();
+
     }
 
     init = () => {
@@ -41,6 +42,7 @@ export default class World {
     handleRendererConfigurations = () => {
         // NOTE - Color Management config
         THREE.ColorManagement.enabled = true;
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
 
         // NOTE - WebGL Renderer Config
         this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -48,8 +50,8 @@ export default class World {
     }
 
     handleControlsConfigurations = () => {
-        // this.controls.maxDistance = MAX_DISTANCE;
-        // this.controls.minDistance = MIN_DISTANCE;
+        this.controls.maxDistance = MAX_DISTANCE;
+        this.controls.minDistance = MIN_DISTANCE;
         this.controls.enableDamping = ENABLE_DAMPING;
         this.controls.enablePan = ENABLE_PAN;
         this.controls.dampingFactor = DAMPING_FACTOR;
